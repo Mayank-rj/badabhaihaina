1d8f3f2250c37b277b4fb44ab9c8f618883c7f05
Mayank-rj <Mayank.kumar.rajoria@gmail.com>
2024-12-13 19:52:10 +0530

    header section updated

--- src/assets/sendEmail.js
+++ src/assets/sendEmail.js
@@ -1,3 +1,23 @@
+import emailjs from '@emailjs/browser';
+import { EnquiryModal } from '../Components/EnquiryModal/EnquiryModal';
 export const sendEmail = (data) => {
     console.log(data);
+    emailjs
+    .sendForm('service_ze691is', 'template_jz4i0fy', data, {
+      publicKey: 's77962P56QvjtWbYa',
+    })
+    .then(
+      () => {
+        console.log('SUCCESS!');
+      },
+      (error) => {
+        console.log('FAILED...', error.text);
+      },
+    );
 }
+// EnquiryModal
+// contatct.js
+// home.jsx
+
+
+
