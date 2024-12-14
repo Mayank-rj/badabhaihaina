f8a4acc628350f686957f288a38e449e6068a467
Mayank-rj <Mayank.kumar.rajoria@gmail.com>
2024-12-14 21:29:55 +0530

    email setup

--- src/assets/sendEmail.js
+++ src/assets/sendEmail.js
@@ -1,23 +1,20 @@
-import emailjs from '@emailjs/browser';
-import { EnquiryModal } from '../Components/EnquiryModal/EnquiryModal';
+import emailjs from "@emailjs/browser";
+import { EnquiryModal } from "../Components/EnquiryModal/EnquiryModal";
 export const sendEmail = (data) => {
-    console.log(data);
-    emailjs
-    .sendForm('service_ze691is', 'template_jz4i0fy', data, {
-      publicKey: 's77962P56QvjtWbYa',
+  console.log(data);
+  emailjs
+    .sendForm("service_2tuirk8", "template_m0ntm39", data, {
+      publicKey: "0BxUCYCAPfCupk8_6",
     })
     .then(
       () => {
-        console.log('SUCCESS!');
+        console.log("SUCCESS!");
       },
       (error) => {
-        console.log('FAILED...', error.text);
-      },
+        console.log("FAILED...", error.text);
+      }
     );
-}
+};
 // EnquiryModal
 // contatct.js
 // home.jsx
-
-
-
